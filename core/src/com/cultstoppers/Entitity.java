6e303f2cbf2c3e5b6e819397f8f41fa9d42c5c66
leonliang33 <llian008@fiu.edu>
2016-01-29 22:10:40 -0500

    Update health and speed

--- core/src/com/cultstoppers/Entitity.java
+++ core/src/com/cultstoppers/Entitity.java
@@ -6,7 +6,7 @@ import com.badlogic.gdx.graphics.Texture;
  * Created by Leon on 1/29/2016.
  */
 public class Entitity {
-    int health;
+    int health1;
     int x,y;
     Texture img;
 }
--- core/src/com/cultstoppers/Player.java
+++ core/src/com/cultstoppers/Player.java
@@ -11,8 +11,21 @@ import com.badlogic.gdx.graphics.g2d.SpriteBatch;
  */
 public class Player extends Entitity{
     int speed=10;
+    int health1=100;
+    public static void PlayerStats(){
+    }
+
     public void move(){
         if(Gdx.input.isKeyPressed(Input.Keys.W)){
+            y=speed;
+        }
+        if(Gdx.input.isKeyPressed(Input.Keys.A)){
+            x=speed;
+        }
+        if(Gdx.input.isKeyPressed(Input.Keys.S)){
+            y=speed;
+        }
+        if(Gdx.input.isKeyPressed(Input.Keys.D)){
             x=speed;
         }
     }
